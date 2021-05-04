2bc94c61f56075ce582ef61c53a925aca5a27182
wghaen2681 <wghaen2681@gmail.com>
2021-05-04 14:57:17 +0800

    20210504
    更新 cheerio 設定

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import linebot from 'linebot'
 import dotenv from 'dotenv'
 import axios from 'axios'
+import cheerio from 'cheerio'
 
 // 讓套件讀取 .env 檔案
 // 讀取後可以用 process.env.變數 使用
@@ -19,15 +20,14 @@ bot.listen('/', process.env.PORT, () => {
 bot.on('message', async event => {
   if (event.message.type === 'text') {
     try {
-      const response = await axios.get('https://datacenter.taichung.gov.tw/swagger/OpenData/f116d1db-56f7-4984-bad8-c82e383765c0')
-      const data = response.data.filter(data => {
-        return data['花種'] === event.message.text
-      })
-
+      const response = await axios.get('https://www.taiwan.net.tw/m1.aspx?sNo=0001019&page=1')
+      const $ = cheerio.load(response.data)
       let reply = ''
-      for (const d of data) {
-        reply += `地點:${d['地點']} \n地址:${d['地址']} \n觀賞時期:${d['觀賞時期']} \n\n`
-      }
+      $('.columnBlock-title').each(function () {
+        console.log($(this).text())
+        reply += $(this).text() + '\n'
+      })
+      console.log(reply)
       event.reply(reply)
     } catch (error) {
       console.log(error)
@@ -35,3 +35,13 @@ bot.on('message', async event => {
     }
   }
 })
+
+// const data = response.data.filter(data => {
+//   return data['花種'] === event.message.text
+// })
+
+// let reply = ''
+// for (const d of data) {
+//   reply += `地點:${d['地點']} \n地址:${d['地址']} \n觀賞時期:${d['觀賞時期']} \n\n`
+// }
+// ..
